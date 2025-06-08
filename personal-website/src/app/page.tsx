4ecc2b46e11369b4e5d69de9a575b1a91b395182
Jacob O. Smith <jacobsmith95@gmail.com>
2025-06-08 11:14:51 -0700

    tailwind experimentation

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <header>
+        <h1 className="flex text-xl items-center justify-items-center">
+          Jacob Smith
+        </h1>
+      </header>
+      <main className="flex flex-col gap-[32px] row-start-2 flex-wrap items-center justify-items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -25,7 +30,7 @@ export default function Home() {
           </li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center justify-items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://google.com/"
