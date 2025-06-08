6068bfdba9014807f33939942d6f6c4ca2ea4a90
Jacob O. Smith <jacobsmith95@gmail.com>
2025-06-08 14:17:14 -0700

    specified header1 font

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -25,22 +25,14 @@ export default function Home() {
           </div>
         </nav>
       </header>
-      <div>
-        <h1>
-          
+      <div className="grid grid-rows-[20px_1fr_px] items-center justify-items-center">
+        <h1 className="font-[Savate] tracking-widest">
+          Designed. Built. Loved.
         </h1>
       </div>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-[32px] row-start-2 flex-wrap sm:items-start">
-        <Image
-          className="float-left dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
@@ -60,13 +52,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             Google
           </a>
           <a
