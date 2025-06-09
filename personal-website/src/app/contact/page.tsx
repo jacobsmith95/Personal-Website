c1af95808c90c17dadf705f77e186ebab3ce9e5a
Jacob O. Smith <jacobsmith95@gmail.com>
2025-06-09 08:11:45 -0700

    removed link sizing

--- personal-website/src/app/contact/page.tsx
+++ personal-website/src/app/contact/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="font-[Savate]">
         <div className="grid grid-cols-2 gap-6 m-64 text-center text-4xl justify-center">
-            <a target="_blank" href="https://github.com/jacobsmith95" className="size-64 bg-black rounded" rel="noopener noreferrer">Github</a>
-            <a target="_blank" href="https://www.linkedin.com/in/jacob-smith-53233279" className="size-64 bg-black rounded" rel="noopener noreferrer">LinkedIn</a>
-            <a target="_blank" href="https://dev.to/jacobsmith95" className="size-64 bg-black rounded" rel="noopener noreferrer">DEV</a>
-            <div className="size-64 bg-black rounded">07</div>
+            <a target="_blank" href="https://github.com/jacobsmith95" className=" bg-gray-600 rounded" rel="noopener noreferrer">Github</a>
+            <a target="_blank" href="https://www.linkedin.com/in/jacob-smith-53233279" className=" bg-black rounded" rel="noopener noreferrer">LinkedIn</a>
+            <a target="_blank" href="https://dev.to/jacobsmith95" className=" bg-black rounded" rel="noopener noreferrer">DEV</a>
+            <div className=" bg-black rounded">07</div>
         </div>
     </div>
   );
