35848b57d38458834cc9dfe8fb6f039be62d6e10
Jacob O. Smith <jacobsmith95@gmail.com>
2025-08-23 13:36:37 -0700

    trying to add css styling

--- personal-website/next.config.ts
+++ personal-website/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: isGitHubPages ? 'https://<Account-name>/<Repository-name>' : '',
   output: "export",
   reactStrictMode: true,
 };
